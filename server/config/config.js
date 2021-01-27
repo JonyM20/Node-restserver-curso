744bf341b9789f3bc13071a02e7a14728c3bb601
Jonatan <jonymerlo14@gmail.com>
2021-01-27 14:52:50 -0300

    Variable de entorno MONGO

--- server/config/config.js
+++ server/config/config.js
@@ -21,7 +21,7 @@ let urlDB;
 if ( process.env.NODE_ENV === 'dev'){
     urlDB = 'mongodb://localhost:27017/cafe'  //Url de coneccion local
 }else{
-    urlDB = 'mongodb+srv://Strider:MmTPf63enpv8AJyB@cluster0.p384v.mongodb.net/cafe' //Url de coneccion remota
+    urlDB = process.env.MONGO_URI //Url de coneccion remota
 }
 
 process.env.URLDB = urlDB;
